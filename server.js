cae543cfc09606908da8bfce1627630c0f812248
davebecoding <d01001010@csu.fullerton.edu>
2022-05-16 18:46:02 -0700

    set up post request for API, set up validation for name, email, and password, set up errors and response network response code(s)

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app = express();
 // DataBase
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
 
 // define routes
