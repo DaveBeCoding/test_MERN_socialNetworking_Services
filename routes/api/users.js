f109b68276e7a03a13b305cab3807470068f75c3
davebecoding <d01001010@csu.fullerton.edu>
2022-05-17 21:24:36 -0700

    checking if user exists, setting up Gravatar, password, and web tokens

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
+const User = require('../../models/User');
 
 // @route POST api/users
 // @desc Register user
@@ -15,7 +16,8 @@ router.post(
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+
+  async (req, res) => {
     // console.log(req.body);
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -23,15 +25,20 @@ router.post(
     }
 
     const { name, email, password } = req.body;
-    // Does user exists
 
-    // Get user Gravatar
-
-    // Encrypt password
-
-    //Return Jsonwebtoken
-
-    res.send('User route');
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+      }
+      // Get user Gravatar
+      // Encrypt password
+      //Return Jsonwebtoken
+      res.send('User route');
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send('Server Error');
+    }
   }
 );
 
