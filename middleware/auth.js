3d1600d8d7da6b3ebe581b37ae7f4b6819033dec
davebecoding <d01001010@csu.fullerton.edu>
2022-05-19 10:57:39 -0700

    setting up middleware authentication, and functions

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,14 @@
 const jwt = require('jsonwebtokem');
 const config = require('config');
 
-module.exports = function (req, res, next) {};
+module.exports = function (req, res, next) {
+  //token header
+  const token = req.header('x-auth-token');
+
+  // confirm token exists
+  if (!token) {
+    return res
+      .status(401)
+      .json({ msg: 'No token found, authorization denied' });
+  }
+};
