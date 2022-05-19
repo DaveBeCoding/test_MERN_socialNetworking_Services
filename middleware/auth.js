ca4382adf8e44bda39652f1eaa55afb471f35667
davebecoding <d01001010@csu.fullerton.edu>
2022-05-19 11:05:08 -0700

    check for existence of token, as well as if it is valid or not

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,10 +5,19 @@ module.exports = function (req, res, next) {
   //token header
   const token = req.header('x-auth-token');
 
-  // confirm token exists
+  // confirm token exists, or if the route is protected
   if (!token) {
     return res
       .status(401)
       .json({ msg: 'No token found, authorization denied' });
+  }
+
+  //There is a token but not valid
+  try {
+    const decoded = jwt.verify(token, config.get('jwtSecret'));
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: 'Token is not valid' });
   }
 };
